80f678c128d4eb2a9f38629c2e54309ad8b92bb2
Asharudeen <asharudeenalikt@gmail.com>
2024-07-13 19:52:33 +0530

    [update] routes endpoint

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -4,9 +4,9 @@ const { BACKEND_URL } = env;
 
 export const API_ROUTES = {
   AUTH: {
-    GENERATE_OTP_POST: `${BACKEND_URL}/otp/generate`,
-    VALIDATE_OTP_POST: `${BACKEND_URL}/otp/validate`,
-    LOGIN_POST: `${BACKEND_URL}/login`,
-    GOOGLE_AUTH_POST: `${BACKEND_URL}/oauth/google`
+    GENERATE_OTP_POST: `${BACKEND_URL}/api/auth/otp/generate`,
+    VALIDATE_OTP_POST: `${BACKEND_URL}/api/auth/otp/validate`,
+    LOGIN_POST: `${BACKEND_URL}/api/auth/login`,
+    GOOGLE_AUTH_POST: `${BACKEND_URL}/api/auth/oauth/google`
   },
 };
